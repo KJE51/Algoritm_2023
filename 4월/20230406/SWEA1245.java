fdd302c43a6bdb37c14673b79f4d9c1fd3b2e028
unknown <kjenotn@gmail.com>
2023-04-07 09:56:37 +0900

    20230406 수정

--- 4월/20230406/SWEA1245.java
+++ 4월/20230406/SWEA1245.java
@@ -1,83 +1,69 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+ 
 public class SWEA1245 {
-
-	static int n, arr[][];
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int testCase = Integer.parseInt(br.readLine())+1;
-		String[] str;
-		StringBuilder sb = new StringBuilder();
-		double left, right, mid=0;
-		double leftW;
-		
-		for(int tc = 1; tc < testCase; tc++) {
-			sb.append('#').append(tc);
-			n = Integer.parseInt(br.readLine());
-			arr= new int[n][2];
-			str = br.readLine().split(" ");
-			
-			//위치들 한번에 입력받은 후, 질량 한번에 입력받기
-			for(int i = 0; i < n; i++) {
-				arr[i][0] = Integer.parseInt(str[i]);
-				arr[i][1] = Integer.parseInt(str[i+n]);
-			}
-			
-			//각 구간별 서치
-			for(int i = 1; i < n; i++) {
-				//각 구간에 대한 이분탐색
-				left = arr[i-1][0];
-				right = arr[i][0];
-				while(left < right && right-left > 1e-12) {
-					mid = (left + right)/2;
-					System.out.println(mid);
-					
-//					res = cal(mid, i);
-					
-					leftW = 0;
-					
-					for(int i1 = 0; i1 < i; i1++) {
-						leftW += arr[i1][1]/((arr[i1][0]-mid)*(arr[i1][0]-mid));
-					}
-					
-					for(int i1 = i; i1 < n; i1++) {
-						leftW -= arr[i1][1]/((arr[i1][0]-mid)*(arr[i1][0]-mid));
-					}
-//					res = leftW;
-					
-					if(leftW == 0) {
-						break;
-					}
-					//왼쪽이 힘이 더 클 때->더 왼쪽 탐색
-					if(leftW < 0) {
-						right = mid;
-					}else {
-					//오른쪽이 더 클 때
-						left = mid;
-					}
-				}
-				sb.append(" ").append(String.format("%.10f", mid));
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb);
-	}
-	
-//	//계산하기
-//	private static double cal(double mid, int i) {
-//		double leftW, rightW;
-//		leftW = rightW = 0;
-//		
-//		for(int i1 = 0; i1 < i; i1++) {
-//			leftW += arr[i1][1]/((arr[i1][0]-mid)*(arr[i1][0]-mid));
-//		}
-//		
-//		for(int i1 = i; i1 < n; i1++) {
-//			rightW += arr[i1][1]/((arr[i1][0]-mid)*(arr[i1][0]-mid));
-//		}
-//		
-//		return leftW - rightW;
-//	}
-
+ 
+    static int n, arr[][];
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine())+1;
+        String[] str;
+        StringBuilder sb = new StringBuilder();
+        double left, right, mid=0;
+        double res;
+        int cnt;
+         
+        for(int tc = 1; tc < testCase; tc++) {
+            sb.append('#').append(tc);
+            n = Integer.parseInt(br.readLine());
+            arr= new int[n][2];
+            str = br.readLine().trim().split(" ");
+             
+            //위치들 한번에 입력받은 후, 질량 한번에 입력받기
+            for(int i = 0; i < n; i++) {
+                arr[i][0] = Integer.parseInt(str[i]);
+                arr[i][1] = Integer.parseInt(str[i+n]);
+            }
+             
+            //각 구간별 서치
+            for(int i = 1; i < n; i++) {
+                //각 구간에 대한 이분탐색
+                left = arr[i-1][0];
+                right = arr[i][0];
+                cnt = 0;
+                while(left < right && cnt <101) {
+                    cnt++;
+                    mid = (left + right)/2;
+                    res = cal(mid, i);
+                    if(res == 0) {
+                        break;
+                    }
+                    //오른쪽 힘이 더 클 때->더 왼쪽으로 가야함
+                    if(res < 0) {
+                        right = mid;
+                    }else {
+                    //왼쪽이 더 클 때->더 오른쪽으로 가야함
+                        left = mid;
+                    }
+                }
+                sb.append(" ").append(String.format("%.10f", mid));
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+     
+    //왼쪽과 오른쪽의 인력 합 계산하기
+    private static double cal(double mid, int idx) {
+        double left, right;
+        left = right = 0;
+        for(int i = 0; i < idx; i++) {
+            left += arr[i][1]/((arr[i][0]-mid)*(arr[i][0]-mid));
+        }
+        for(int i = idx; i < n; i++) {
+            right += arr[i][1]/((arr[i][0]-mid)*(arr[i][0]-mid));
+        }
+        return left - right;
+    }
+ 
 }
--- 4월/20230406/SWEA5607.java
+++ 4월/20230406/SWEA5607.java
@@ -1,9 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class SWEA5607 {
+	
+	static long[] fac;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int testCase = Integer.parseInt(br.readLine())+1;
+		String[] str;
+		int n, r, q = 1234567891;
+		
+		for(int tc = 1; tc < testCase; tc++) {
+			str = br.readLine().split(" ");
+			n = Integer.parseInt(str[0]);
+			r = Integer.parseInt(str[1]);
+			
+			// 팩토리얼 미리 계산하기
+			fac = new long[n+1];
+			fac[0] = 1;
+			for(int i = 1; i <= n; i++)
+				fac[i] = fac[i-1] * i % q;
+			
+			//nCr 계산해서 출력하기
+			System.out.printf("#%d %d\n", tc, cal(n, r, q));
+		}
+	}
 
+	//nCr 계산하는 메소드
+	private static long cal(int n, int r, int p) {
+		if(r == 0)
+			return 1L;
+		return (fac[n] * power(fac[r], p-2, p) % p * power(fac[n-r], p-2, p)%p)%p;
+	}
+
+	//거듭제곱 계산하는 메소드
+	private static long power(long x, long y, long p) {
+		long res = 1L;
+		x = x % p;
+		while(y > 0) {
+			if(y % 2 == 1)
+				res = (res * x) % p;
+			y = y >> 1;
+			x = (x * x) % p;
+		}
+		return res;
 	}
 
 }
